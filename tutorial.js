33933d47f0a137dc5e73e6c6c6c27489a3f84a7a
Michael Heinisch <venomx411@gmail.com>
2018-04-19 20:18:18 -0500

    learned about the EventEmitter class

--- tutorial.js
+++ tutorial.js
@@ -49,3 +49,14 @@ fs.readdir('./', function (err, files) {
 
 //////////////////////////////////////////
 // Below, we will explore the events module
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on('messageLogged', function () {
+  console.log('Listener called');
+});
+
+// Raise an event
+emitter.emit('messageLogged');
